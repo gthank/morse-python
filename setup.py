47825bcda7f9aeebe12f11f873df0c99634f7ff1
Hank Gay <hank.gay@gmail.com>
2012-09-12 01:07:19 -0400

    Tweak setup.py to reflect new project structure.

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from distutils.core import setup
 setup(
     name='python-morse',
     version='1.1',
-    packages=['python-morse',],
+    packages=['python-morse', 'python-morse.test'],
+    scripts=['bin/cycle.sh', 'bin/example.bat', 'bin/rndwords.bat'],
     license='GPLv2',
+    description='A Python library for Morse code.',
     long_description=open('README.md').read(),
     author='Christian Höltje',
     author_email='docwhat@gerf.org',
