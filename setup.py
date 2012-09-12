545abd26c17dc1f8a7f039bf8c41fde506bc190f
Hank Gay <hank.gay@gmail.com>
2012-09-12 00:46:34 -0400

    Start factoring into packaging-friendly layout.

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- python; coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='python-morse',
+    version='1.1',
+    packages=['python-morse',],
+    license='GPLv2',
+    long_description=open('README.md').read(),
+    author='Christian Höltje',
+    author_email='docwhat@gerf.org',
+    url='https://github.com/docwhat/morse-python'
+)
+
